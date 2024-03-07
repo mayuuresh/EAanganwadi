d62e0ece5f27fa257b4a439a73553ef9389368b8
DurevshChaudhari <durveshchaudhari11@gmail.com>
2024-03-08 00:47:29 +0530

    new_form_java_done

---
+++ app/src/main/java/com/example/aahaarapp/Issue.java
@@ -0,0 +1,12 @@
+package com.example.aahaarapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Issue extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.issue_form);
+    }
+}
