cf704340111ddd3bc8bc5e1785f8b27f81ca41ff
DurevshChaudhari <durveshchaudhari11@gmail.com>
2024-03-08 10:54:14 +0530

    Phase_1_Done

---
+++ app/src/main/java/com/example/aahaarapp/Preganent_Register.java
@@ -0,0 +1,13 @@
+package com.example.aahaarapp;
+
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Preganent_Register extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pregnant_register);
+    }
+}
---
+++ app/src/main/java/com/example/aahaarapp/Preganent_add.java
@@ -0,0 +1,14 @@
+package com.example.aahaarapp;
+
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+class Preganent_add extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pregnent_add);
+    }
+}
+
