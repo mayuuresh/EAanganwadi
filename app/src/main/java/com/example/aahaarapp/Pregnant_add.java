2ec6978ff7759f5b9e8d64d982bdc88586ee6907
mayuresh <115038126+Mayureshh2004@users.noreply.github.com>
2024-03-08 11:41:36 +0530

    pregnent_add_final

---
+++ app/src/main/java/com/example/aahaarapp/Pregnant_add.java
@@ -0,0 +1,22 @@
+package com.example.aahaarapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Pregnant_add extends AppCompatActivity {
+
+    @Override
+    public void onCreate( Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    setContentView(R.layout.pregnant_add);
+
+
+    }
+//        public void back(View v) {
+//            Intent intent = new Intent(this, MainActivity.class);
+//            startActivity(intent);
+//        }
+
+    }
+
