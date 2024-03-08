144f6fb09c0162df6750a552abf85b8514f82523
mayuresh <115038126+Mayureshh2004@users.noreply.github.com>
2024-03-08 10:53:08 +0530

    pregnent_add_final

--- app/src/main/java/com/example/aahaarapp/Pregnent_add.java
+++ app/src/main/java/com/example/aahaarapp/Pregnent_add.java
@@ -2,8 +2,12 @@ package com.example.aahaarapp;
 
 import android.app.ActionBar;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.Toolbar;
 
 import androidx.annotation.NonNull;
@@ -14,7 +18,11 @@ public class Pregnent_add extends AppCompatActivity {
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState);}
+        super.onCreate(savedInstanceState);
+    setContentView(R.layout.pregnent_add);
+
+
+    }
         public void back(View v) {
             Intent intent = new Intent(this, MainActivity.class);
             startActivity(intent);
