b878dbe720e09660dd387f754239a7030bfdde90
DurevshChaudhari <durveshchaudhari11@gmail.com>
2024-03-10 02:08:02 +0530

    read data

--- app/src/main/java/com/example/aahaarapp/LactatingMother_add.java
+++ app/src/main/java/com/example/aahaarapp/LactatingMother_add.java
@@ -1,6 +1,7 @@
 package com.example.aahaarapp;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,10 +11,13 @@ import androidx.appcompat.app.AppCompatActivity;
 public class LactatingMother_add extends AppCompatActivity {
 
     Button btn;
+    MyDBHelperLactatingMother helper;
     public void onCreate( Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.lactating_add);
         btn=findViewById(R.id.button6);
+        helper = new MyDBHelperLactatingMother(this);
+        Cursor cursor = helper.readAllData();
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
--- app/src/main/java/com/example/aahaarapp/MyDBHelperLactatingMother.java
+++ app/src/main/java/com/example/aahaarapp/MyDBHelperLactatingMother.java
@@ -2,6 +2,7 @@ package com.example.aahaarapp;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
 import android.database.sqlite.SQLiteOpenHelper;
@@ -74,5 +75,18 @@ public class MyDBHelperLactatingMother extends SQLiteOpenHelper {
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
 
+    }
+
+    public Cursor readAllData()
+    {
+        String query = "SELECT * FROM " + TABLE_NAME_Lac;
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        Cursor cursor = null;
+        if(db != null)
+        {
+            cursor = db.rawQuery(query,null);
+        }
+        return cursor;
     }
 }
