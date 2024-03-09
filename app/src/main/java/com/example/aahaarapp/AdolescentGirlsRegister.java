96f09ae28d52bdc7d6b1081ee2e55f241e543be4
mayuresh <115038126+Mayureshh2004@users.noreply.github.com>
2024-03-10 00:20:20 +0530

    Adolescent_Database

--- app/src/main/java/com/example/aahaarapp/AdolescentGirlsRegister.java
+++ app/src/main/java/com/example/aahaarapp/AdolescentGirlsRegister.java
@@ -1,14 +1,63 @@
 package com.example.aahaarapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class AdolescentGirlsRegister extends AppCompatActivity {
+public class AdolescentGirlsRegister extends AppCompatActivity
+{
+    EditText mobile1,name1,dob1,yob1,mother1,father1;
+    Button register;
+    String email,pass,repass;
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState) ;
 
-        setContentView(R.layout.adolescent_boys_register);
-    }
+        setContentView(R.layout.adolescent_girls_register);
+        mobile1 =findViewById(R.id.mobile);
+        name1 = findViewById(R.id.Name);
+        dob1 = findViewById(R.id.Dob);
+        yob1 = findViewById((R.id.Yob));
+        register=findViewById((R.id.Register));
+
+        try (
+                MyDBHelperAdolescentGirls helper = new MyDBHelperAdolescentGirls(this)){
+
+
+
+            register.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    String mobile = mobile1.getText().toString();
+                    String name = name1.getText().toString();
+                    String dob = dob1.getText().toString();
+                    String yob = yob1.getText().toString();
+                    String father=father1.getText().toString();
+                    String mother=mother1.getText().toString();
+
+
+                    // Now, you have the text of the checked RadioButtons
+                    // Proceed with further processing
+
+                    helper.registeruser(name, dob, yob, mobile, father,mother);
+
+                    Toast.makeText(AdolescentGirlsRegister.this, "Data Saved Successfully", Toast.LENGTH_SHORT).show();
+
+
+                    Intent intent = new Intent(getApplicationContext(), Logup.class);
+                    startActivity(intent);
+
+
+                }
+            });
+        }catch(Exception ignored)
+        {
+
+        }}
 }
---
+++ app/src/main/java/com/example/aahaarapp/MyDbHelperAdolescentBoys.java
@@ -0,0 +1,54 @@
+package com.example.aahaarapp;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+
+public class MyDbHelperAdolescentBoys extends SQLiteOpenHelper {
+
+    private static final String DB_NAME = "AdolescentGirls";
+    private static final int DB_VERSION = 1;
+
+    private static final String TABLE_NAME = "Adolescentgirls";
+    private static final String name = "Name";
+    private static final String dob = "Dob";
+    private static final String yob = "Yob";
+    private static final String father = "Father";
+    private static final String mother = "Mother";
+
+    private static final String mobileNo = "Mobile_No";
+    public MyDbHelperAdolescentBoys(@Nullable Context context) {
+        super(context,DB_NAME,null, DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String query_reg = "CREATE TABLE " + TABLE_NAME + " ("
+                + mobileNo + " TEXT PRIMARY KEY, "
+                + name + "TEXT" + dob + "TEXT" + yob + "TEXT"
+                + father + " TEXT"+mother+"TEXT)";
+        db.execSQL(query_reg);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+    public void registeruser(String name1,String dob1,String yob1,String mobile1,String father,String mother)
+    {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(mobileNo, mobile1);
+        values.put(name,name1 );
+        values.put(dob,dob1);
+        values.put(yob,yob1);
+        values.put(father,father);
+        values.put(mother,mother);
+        db.insert(TABLE_NAME, null, values);
+        db.close();
+    }
+}
