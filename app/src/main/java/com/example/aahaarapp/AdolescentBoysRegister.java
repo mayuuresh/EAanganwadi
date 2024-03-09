2b596d48ebe801ba338ae7bf4bca639d3060fe46
mayuresh <115038126+Mayureshh2004@users.noreply.github.com>
2024-03-09 23:22:25 +0530

    Adolescent

---
+++ app/src/main/java/com/example/aahaarapp/AdolescentBoysRegister.java
@@ -0,0 +1,14 @@
+package com.example.aahaarapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AdolescentBoysRegister extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState) ;
+
+        setContentView(R.layout.adolescent_boys_register);
+    }
+}
---
+++ app/src/main/java/com/example/aahaarapp/AdolescentGirlsAdd.java
@@ -0,0 +1,14 @@
+package com.example.aahaarapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AdolescentGirlsAdd extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState) ;
+
+        setContentView(R.layout.adolescent_girls_add);
+    }
+}
