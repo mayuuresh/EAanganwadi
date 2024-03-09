914ab3939acef8383e5928cb027e0d16fc7dcaaf
SalunkeYash <yashsalunke555@gmail.com>
2024-03-10 01:46:38 +0530

    child3y6yRegister update

--- app/src/main/java/com/example/aahaarapp/Children3Y6YNutrition.java
+++ app/src/main/java/com/example/aahaarapp/Children3Y6YNutrition.java
@@ -20,6 +20,7 @@ public class Children3Y6YNutrition extends AppCompatActivity {
     RadioButton r;
     Button btn;
     MyDBHelperChildren3y6yRegister helper;
+    String mobileno="",nurtrition="",service="";
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.children_3y_6y_nutrition);
@@ -28,6 +29,7 @@ public class Children3Y6YNutrition extends AppCompatActivity {
         Iron1 = findViewById(R.id.Iron1);
         Vitamin1 = findViewById(R.id.Vitamin1);
         Calcium1 = findViewById(R.id.Calcium1);
+
         allopath = findViewById(R.id.allopath);
         homopathy = findViewById(R.id.homopathy);
         ayush = findViewById(R.id.ayush);
@@ -37,7 +39,36 @@ public class Children3Y6YNutrition extends AppCompatActivity {
         fat = findViewById(R.id.fat);
         hemoglobin = findViewById(R.id.hemoglobin);
 
-        String mobileno;
+
+        if(FalicAcid1.isSelected())
+        {
+            nurtrition=nurtrition+","+(FalicAcid1.getText().toString());
+        }
+        if(Iron1.isSelected())
+        {
+            nurtrition=nurtrition+","+(Iron1.getText().toString());
+        }
+        if(Vitamin1.isSelected())
+        {
+            nurtrition=nurtrition+","+(Vitamin1.getText().toString());
+        }
+        if(Calcium1.isSelected())
+        {
+            nurtrition=nurtrition+","+(Calcium1.getText().toString());
+        }
+
+        if(allopath.isSelected())
+        {
+            service=service+","+(allopath.getText().toString());
+        }
+        if(homopathy.isSelected())
+        {
+            service=service+","+(homopathy.getText().toString());
+        }
+        if (ayush.isSelected())
+        {
+            service=service+","+(ayush.getText().toString());
+        }
 
         radio = findViewById(R.id.radioheight);
 
@@ -60,14 +91,14 @@ public class Children3Y6YNutrition extends AppCompatActivity {
                     r = findViewById(selectedId);
                     String radion = r.getText().toString();
 
-                    helper.updateColumns(mobileno,);
+                    helper.updateColumns(mobileno,nurtrition,heightn,weightn,fatn,radion,hemoglobinn,service);
                     Toast.makeText(Children3Y6YNutrition.this, "Data Saved Successfully", Toast.LENGTH_SHORT).show();
 
                     Intent intent = new Intent(getApplicationContext(), Logup.class);
                     startActivity(intent);
                 } else {
                     // Handle the case where no RadioButton is selected
-                    Toast.makeText(Children3Y6YNutrition.this, "Please select gender", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(Children3Y6YNutrition.this, "Please Select ALl the Fields", Toast.LENGTH_SHORT).show();
                 }
             }
         });
